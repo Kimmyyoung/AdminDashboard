55a53626664a6c30e390dba637d221d566dc6d28
hykim <hiyoung.kimmy@gmail.com>
2023-09-13 21:51:25 -0700

    ✨ feat: Create Header

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Box } from '@mui/material';
+
+
+import Header from '../../components/Header';
 
 const Dashboard = () => {
   return (
-    <div>index</div>
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subtitle="Welcome to the Dashboard"></Header>
+      </Box>
+    </Box>
   )
 }
 
