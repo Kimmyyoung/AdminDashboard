2a467167e6bef326f5c396a59ef8c2fb675790c4
hykim <hiyoung.kimmy@gmail.com>
2023-09-15 17:29:30 -0700

    ✨ feat: Add DataGrid from MUI

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Topbar from './scenes/global/Topbar';
 import Sidebar from './scenes/global/Sidebar';
 
 import Dashboard from './scenes/dashboard';
-// import Team from './scenes/team';
-// import Invoices from './scenes/invoices';
-// import Contacts from './scenes/contacts';
+import Team from './scenes/team';
+import Invoices from './scenes/invoices';
+import Contacts from './scenes/contacts';
 // import Bar from './scenes/bar';
 // import Form from './scenes/form';
 // import Line from './scenes/line';
@@ -31,10 +31,10 @@ function App() {
             <Topbar />
             <Routes>
               <Route path='/' element={<Dashboard />} />
-              {/* <Route path='/team' element={<Team />} />
-              <Route path='/invoices' element={<Invoices />} />
+              <Route path='/team' element={<Team />} />
               <Route path='/contacts' element={<Contacts />} />
-              <Route path='/bar' element={<Bar />} />
+              <Route path='/invoices' element={<Invoices />} />
+              {/* <Route path='/bar' element={<Bar />} />
               <Route path='/form' element={<Form />} />
               <Route path='/line' element={<Line />} />
               <Route path='/pie' element={<Pie />} />
