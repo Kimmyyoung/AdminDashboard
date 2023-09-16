c28deafafdcb447a9170fefd695ede6d17f4f2af
hykim <hiyoung.kimmy@gmail.com>
2023-09-16 12:20:21 -0700

    ✨ feat: Add Calendar - FullCalendar Library

--- src/App.js
+++ src/App.js
@@ -9,13 +9,14 @@ import Dashboard from './scenes/dashboard';
 import Team from './scenes/team';
 import Invoices from './scenes/invoices';
 import Contacts from './scenes/contacts';
-// import Bar from './scenes/bar';
 import Form from './scenes/form';
+import Calendar from './scenes/calendar';
+
+// import Bar from './scenes/bar';
 // import Line from './scenes/line';
 // import Pie from './scenes/pie';
 // import FAQ from './scenes/faq';
 // import Geography from './scenes/geography';
-// import Calendar from './scenes/Calendar';
 
 
 function App() {
@@ -34,13 +35,15 @@ function App() {
               <Route path='/team' element={<Team />} />
               <Route path='/contacts' element={<Contacts />} />
               <Route path='/invoices' element={<Invoices />} />
-              {/* <Route path='/bar' element={<Bar />} /> */}
               <Route path='/form' element={<Form />} />
-              {/* <Route path='/line' element={<Line />} />
+              <Route path='/calendar' element={<Calendar />} />
+
+              {/* <Route path='/bar' element={<Bar />} /> */}
+              {/* <Route path='/form' element={<Form />} />
+              <Route path='/line' element={<Line />} />
               <Route path='/pie' element={<Pie />} />
               <Route path='/faq' element={<FAQ />} />
-              <Route path='/geography' element={<Geography />} />
-              <Route path='/calendar' element={<Calendar />} /> */}
+              <Route path='/geography' element={<Geography />} /> */}
             </Routes>
           </main>
         </div>
